e85ca4300c7f135b2c49477f0f3afcd88d83242b
xiongxl <xianliang.xiongxl@ymm56.com>
2019-02-26 23:55:27 +0800

    spring study

---
+++ src/test/java/TestBeanScope.java
@@ -0,0 +1,19 @@
+/**
+ * ymm56.com Inc.
+ * Copyright (c) 2013-2019 All Rights Reserved.
+ */
+
+import org.junit.Test;
+
+/**
+ *
+ * @author xiongxl
+ * @version $Id: TestBeanScope.java, v 0.1 2019-02-26 下午11:21 xiongxl Exp $$
+ */
+public class TestBeanScope {
+
+    @Test
+    public void test() {
+        System.out.print("*****");
+    }
+}
